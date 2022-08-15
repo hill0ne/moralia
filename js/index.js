3224bd0851cfd7f19ac17db704d571790ca12cc8
hill0ne <jeanj531@gmail.com>
2022-08-15 13:01:34 +0900

    Feat: "추가 swiper 라이브러리"
    - 라이브러리를 이용해 휠 이벤트 슬라이드 적용
    - 휠 이벤트로 index 페이지로 이동하는 함수 추가

--- js/index.js
+++ js/index.js
@@ -1,38 +1,60 @@
-const sectionVideo = document.querySelector(".section-video");
-const sectionContetns = document.querySelector(".section-contents");
-// const slideList = document.querySelector(".list");
+const header = document.querySelector(".header");
+const title = document.querySelector(".title");
+const video = document.querySelector(".section-video");
+const contents = document.querySelector(".section-contents");
+const slide = document.querySelector(".swiper-slide");
+const page_title = document.querySelector(".page-title");
 const btn = document.querySelector(".test");
 const textWrap = document.querySelector(".text_wrap");
-const title = document.querySelector(".title");
 const footer = document.querySelector(".footer");
 
-// 메인 페이지 이동
-sectionVideo.addEventListener("wheel", pageMoveTopToDown);
+let status = false;
+
+// 마우스 휠로 페이지 이동
+video.addEventListener("wheel", pageMoveTopToDown);
+header.addEventListener("wheel", pageMoveDownToTop);
+footer.addEventListener("wheel", pageMoveDownToTop);
 
 function pageMoveTopToDown(e) {
   e.preventDefault();
   let wheelValue = e.deltaY;
   if (wheelValue > 0) {
-    sectionVideo.classList.add("is-active");
+    video.classList.add("is-active");
     title.classList.add("is-active");
     textWrap.classList.add("is-active");
     footer.classList.add("is-active");
   }
 }
 
-// 스와이퍼 슬라이드 라이브러리
-// var swiper = new Swiper(".mySwiper", {
-//   slidesPerView: 3,
-//   spaceBetween: 30,
-//   slidesPerGroup: 3,
-//   loop: true,
-//   loopFillGroupWithBlank: true,
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-// });
+function pageMoveDownToTop(e) {
+  e.preventDefault();
+  let wheelValue = e.deltaY;
+  if (wheelValue < 0) {
+    video.classList.remove("is-active");
+    title.classList.remove("is-active");
+    textWrap.classList.remove("is-active");
+    footer.classList.remove("is-active");
+  }
+}
+
+contents.addEventListener("wheel", (e) => {
+  let y = e.deltaY;
+  if (y > 0) {
+    console.log();
+  }
+});
+
+// swiper 라이브러리
+var swiper = new Swiper(".slide", {
+  width: 600,
+  direction: "horizontal",
+  slidesPerView: 1,
+  spaceBetween: 350,
+  mousewheel: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  speed: 1500,
+});
